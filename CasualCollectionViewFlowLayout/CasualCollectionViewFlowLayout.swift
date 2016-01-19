ae9750cedb1d65993474e3b0139c51e6772751ac
Lacy Rhoades <lacyrhoades@gmail.com>
2016-01-18 20:58:17 -0500

    Reverting to a simple springy behavior

--- CasualCollectionViewFlowLayout/CasualCollectionViewFlowLayout.swift
+++ CasualCollectionViewFlowLayout/CasualCollectionViewFlowLayout.swift
@@ -1,5 +1,5 @@
 //
-//  CasualHorizontalLayout.swift
+//  CasualCollectionViewFlowLayout.swift
 //  CasualUX
 //
 //  Created by Lacy Rhoades on 11/2/15.
@@ -9,16 +9,9 @@
 import UIKit
 
 class CasualCollectionViewFlowLayout : UICollectionViewFlowLayout {
-    
-    var resistance: CGFloat = 800
-    var length: CGFloat = 0.0
-    var damping: CGFloat = 1.0
-    var frequency: CGFloat = 1.0
-    
     private var dynamicAnimator: UIDynamicAnimator!
-    
     private var visiblePaths: Set<NSIndexPath>!
-    private var latestDelta: CGPoint = CGPointZero
+    private var latestDelta: CGFloat = 0
     
     override init() {
         super.init()
@@ -27,90 +20,11 @@ class CasualCollectionViewFlowLayout : UICollectionViewFlowLayout {
         self.visiblePaths = Set()
     }
     
-    required init(coder aDecoder: NSCoder) {
-        fatalError()
-    }
-    
-    override func prepareLayout() {
-        super.prepareLayout()
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         
-        let scrollView: UIScrollView = self.collectionView!
-        
-        let visibleRect = CGRectInset(scrollView.bounds, -100, -100)
-        let itemsInVisibleRect = super.layoutAttributesForElementsInRect(visibleRect)!
-        
-        let indexPathsInVisibleRect = Set(itemsInVisibleRect.map({ (attribute) -> NSIndexPath in
-            return attribute.indexPath
-        }))
-        
-        
-        // PART 1 - REMOVING any old behaviors as old cells go away
-        
-        let noLongerVisisbleBehaviors = self.dynamicAnimator.behaviors.filter { (behavior) -> Bool in
-            var currentlyVisible = false
-            if let attachmentBehavior = behavior as? UIAttachmentBehavior, last = attachmentBehavior.items.last as? UICollectionViewLayoutAttributes {
-                currentlyVisible = indexPathsInVisibleRect.contains(last.indexPath)
-            }
-            return !currentlyVisible
-        }
-        
-        for behavior in noLongerVisisbleBehaviors {
-            self.dynamicAnimator.removeBehavior(behavior)
-            if let attachmentBehavior = behavior as? UIAttachmentBehavior, last = attachmentBehavior.items.last as? UICollectionViewLayoutAttributes {
-                visiblePaths.remove(last.indexPath)
-            }
-        }
-        
-        
-        // PART 2 - ADDING any new behaviors as new cells appear
-        
-        let newlyVisibleItems = itemsInVisibleRect.filter { (layoutAttribute) -> Bool in
-            return !visiblePaths.contains(layoutAttribute.indexPath)
-        }
-        
-        let touchedCellCenter = self.collectionView!.touchedCellCenter()
-        
-        for item in newlyVisibleItems {
-            var center = item.center
-            
-            let attachmentBehavior = UIAttachmentBehavior(item: item, attachedToAnchor: item.center)
-            
-            attachmentBehavior.length = self.length
-            attachmentBehavior.damping = self.damping
-            attachmentBehavior.frequency = self.frequency
-            
-            let distance = CGPointMake(
-                fabs(touchedCellCenter.x - attachmentBehavior.anchorPoint.x),
-                fabs(touchedCellCenter.y - attachmentBehavior.anchorPoint.y)
-            )
-            
-            let resistance = CGPointMake(
-                distance.x / self.resistance,
-                distance.y / self.resistance
-            )
-
-            var shouldBeSpringy = false
-            if (self.latestDelta.x > 0) {
-                shouldBeSpringy = item.center.x > touchedCellCenter.x
-            } else if (self.latestDelta.x < 0) {
-                shouldBeSpringy = item.center.x < touchedCellCenter.x
-            } else if (self.latestDelta.y > 0) {
-                shouldBeSpringy = item.center.y > touchedCellCenter.y
-            } else if (self.latestDelta.y < 0) {
-                shouldBeSpringy = item.center.y < touchedCellCenter.y
-            }
-            
-            if (shouldBeSpringy) {
-                center.x += self.latestDelta.x * resistance.x
-                center.y += self.latestDelta.y * resistance.y
-            }
-            
-            item.center = center
-            
-            self.dynamicAnimator.addBehavior(attachmentBehavior)
-            
-            self.visiblePaths.insert(item.indexPath)
-        }
+        self.dynamicAnimator = UIDynamicAnimator(collectionViewLayout: self)
+        self.visiblePaths = Set()
     }
     
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
@@ -121,56 +35,110 @@ class CasualCollectionViewFlowLayout : UICollectionViewFlowLayout {
         return self.dynamicAnimator.layoutAttributesForCellAtIndexPath(indexPath)
     }
     
-    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
-        let touchedCellCenter = self.collectionView!.touchedCellCenter()
+    override func layoutAttributesForSupplementaryViewOfKind(elementKind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        return self.dynamicAnimator.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: indexPath)
+    }
+    
+    override func prepareLayout() {
+        super.prepareLayout()
         
         let scrollView: UIScrollView = self.collectionView!
         
-        self.latestDelta = CGPointMake(
-            newBounds.origin.x - scrollView.bounds.origin.x,
-            newBounds.origin.y - scrollView.bounds.origin.y
-        )
+        let visibleRect = CGRectInset(scrollView.bounds, -100, -100)
         
-        for behavior in self.dynamicAnimator.behaviors {
-            let attachmentBehavior = behavior as! UIAttachmentBehavior
+        let itemsInVisibleRect = super.layoutAttributesForElementsInRect(visibleRect)!
         
-            let distance = CGPointMake(
-                fabs(touchedCellCenter.x - attachmentBehavior.anchorPoint.x),
-                fabs(touchedCellCenter.y - attachmentBehavior.anchorPoint.y)
-            )
-            
-            let resistance = CGPointMake(
-                distance.x / self.resistance,
-                distance.y / self.resistance
-            )
-            
-            let item = attachmentBehavior.items.first!
-            
+        let indexPathsInVisibleRect = Set(itemsInVisibleRect.map({ (attribute) -> NSIndexPath in
+            return attribute.indexPath
+        }))
+
+        // PART 1 - REMOVING any old behaviors as old cells go away
+
+        let noLongerVisisbleBehaviors = self.dynamicAnimator.behaviors.filter { (eachBehavior) -> Bool in
+            var currentlyVisible = false
+            if let behavior = eachBehavior as? UIAttachmentBehavior, last = behavior.items.last as? UICollectionViewLayoutAttributes {
+                currentlyVisible = indexPathsInVisibleRect.contains(last.indexPath)
+            }
+            return !currentlyVisible
+        }
+        
+        for eachBehavior in noLongerVisisbleBehaviors {
+            self.dynamicAnimator.removeBehavior(eachBehavior)
+            if let behavior = eachBehavior as? UIAttachmentBehavior, last = behavior.items.last as? UICollectionViewLayoutAttributes {
+                visiblePaths.remove(last.indexPath)
+            }
+        }
+
+        // PART 2 - ADDING any new behaviors as new cells appear
+        
+        let touchLocation = self.collectionView!.touchedCellCenter()
+
+        let newlyVisibleItems = itemsInVisibleRect.filter { (layoutAttribute) -> Bool in
+            return !visiblePaths.contains(layoutAttribute.indexPath)
+        }
+        
+        for item in newlyVisibleItems {
             var center = item.center
             
-            var shouldBeSpringy = false
-            if (self.latestDelta.x > 0) {
-                shouldBeSpringy = item.center.x > touchedCellCenter.x
-            } else if (self.latestDelta.x < 0) {
-                shouldBeSpringy = item.center.x < touchedCellCenter.x
-            } else if (self.latestDelta.y > 0) {
-                shouldBeSpringy = item.center.y > touchedCellCenter.y
-            } else if (self.latestDelta.y < 0) {
-                shouldBeSpringy = item.center.y < touchedCellCenter.y
+            let behavior = UIAttachmentBehavior(item: item, attachedToAnchor: center)
+            
+            behavior.length = 0.0
+            behavior.damping = 0.8
+            behavior.frequency = 1.0
+            
+            if (!CGPointEqualToPoint(CGPointZero, touchLocation)) {
+                let yDistanceFromTouch = fabs(touchLocation.y - behavior.anchorPoint.y)
+                let xDistanceFromTouch = fabs(touchLocation.x - behavior.anchorPoint.x)
+                let scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 1500.0
+                
+                if (self.latestDelta < 0) {
+                    center.x += max(self.latestDelta, self.latestDelta*scrollResistance);
+                }
+                else {
+                    center.x += min(self.latestDelta, self.latestDelta*scrollResistance);
+                }
+                item.center = center;
             }
             
-            if (shouldBeSpringy) {
-                center.x += self.latestDelta.x * resistance.x
-                center.y += self.latestDelta.y * resistance.y
+            self.dynamicAnimator.addBehavior(behavior)
+            self.visiblePaths.insert(item.indexPath)
+        }
+    }
+
+    
+    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
+        let scrollView = self.collectionView! as UIScrollView
+        
+        let delta = newBounds.origin.x - scrollView.bounds.origin.x
+        
+        self.latestDelta = delta
+        
+        let touchLocation = self.collectionView!.touchedCellCenter()
+        
+        for eachBehavior in self.dynamicAnimator.behaviors {
+            if let behavior = eachBehavior as? UIAttachmentBehavior {
+                let yDistanceFromTouch = fabs(touchLocation.y - behavior.anchorPoint.y)
+                let xDistanceFromTouch = fabs(touchLocation.x - behavior.anchorPoint.x)
+                let scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 1500.0
+            
+                let item = behavior.items.first as! UICollectionViewLayoutAttributes
+            
+                var center = item.center
+                
+                if (delta < 0) {
+                    center.x += max(delta, delta*scrollResistance)
+                } else {
+                    center.x += min(delta, delta*scrollResistance)
+                }
+
+                item.center = center
+                
+                self.dynamicAnimator.updateItemUsingCurrentState(item)
             }
-            
-            item.center = center
-            
-            self.dynamicAnimator.updateItemUsingCurrentState(item)
         }
         
         return false
-    }
+    }   
 }
 
 private extension UICollectionView {
